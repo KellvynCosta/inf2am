f61ad1ebcd189ae64bfb071d7ac98ca81e1bdf40
Kellvyn Costa <kellvyn.thiagocosta@gmail.com>
2023-06-13 07:28:06 -0300

    Início controle de versão

---
+++ src/main/java/com/itb/inf2am/model/Produto.java
@@ -0,0 +1,18 @@
+package com.itb.inf2am.model;
+
+public class Produto {
+	
+	private Long id;
+	private String nome;
+	private String descricao;
+	private String CodigoBarras;
+	private double preco;
+	
+	// public : Acesso liberado para todas as classes.
+	
+	// private : Acesso permitido apenas para os membros
+	// 			 (atributos e ou métodos) da própria classe.
+	
+	
+	
+}
