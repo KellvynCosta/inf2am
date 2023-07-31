84071cf3c03a2e0ba9637a8c76eb91a43b074bfb
Kellvyn Costa <kellvyn.thiagocosta@gmail.com>
2023-07-31 11:24:55 -0300

    Início controle de versão

--- src/main/java/com/itb/inf2am/controller/LojaController.java
+++ src/main/java/com/itb/inf2am/controller/LojaController.java
@@ -1,8 +1,14 @@
 package com.itb.inf2am.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.itb.inf2am.model.Produto;
 
 @Controller
 @RequestMapping("/comercio/produtos")
@@ -10,10 +16,43 @@ public class LojaController {
 	
 	
 	@GetMapping("/listar")
-	public String listarProdutos() {
-		System.out.println("LISTA DE PRODUTOS");
+	public String listarProdutos(Model model) {
 		
+		List<Produto> listaDeProdutos = new ArrayList<Produto>();
+		
+		Produto p1 = new Produto();
+		
+		p1.setId(10l);
+		p1.setNome("Máquina de lavar Brastemp 15l");
+		p1.setDescricao("Máquina de Lavar Brastemp 15lcom painel digital");
+		p1.setCodigoBarras("HDAGSJSBDYUDWAK289472");
+		p1.setPreco(3515.64);
+		p1.setCodStatus(false);
+		
+		Produto p2 = new Produto();
+		
+		p2.setId(11l);
+		p2.setNome("Micro-ondas Eletrolux");
+		p2.setDescricao("Micro-ondas Eletrolux 30l auto-limpante");
+		p2.setCodigoBarras("JHAWDDUOAWHDI5835");
+		p2.setPreco(2632.65);
+		p2.setCodStatus(true);
+		
+		
+		listaDeProdutos.add(p1);
+		listaDeProdutos.add(p2);
+		
+		
+		
+		model.addAttribute("listaDeProdutos", listaDeProdutos);
 		return "produtos";
+		
+	}
+	
+	@GetMapping("/novo-produto")
+	public String novoProduto() {
+		
+		return "novo-prod";
 		
 	}
 
--- src/main/java/com/itb/inf2am/model/Produto.java
+++ src/main/java/com/itb/inf2am/model/Produto.java
@@ -7,11 +7,75 @@ public class Produto {
 	private String descricao;
 	private String CodigoBarras;
 	private double preco;
+	private boolean codStatus;
 	
 	// public : Acesso liberado para todas as classes.
 	
 	// private : Acesso permitido apenas para os membros
 	// 			 (atributos e ou métodos) da própria classe.
+
+	
+	// Métodos Setter's e Getter's : Atribuir e recuperar dados
+	// dos atributos "RESPECTIVAMENTE"
+	// Cada atributo possui o seu método set e o seu método get
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	
+	public void setCodigoBarras(String CodigoBarras) {
+		this.CodigoBarras = CodigoBarras;
+	}
+	
+	
+	public String getCodigoBarras() {
+		return CodigoBarras;
+	}
+	
+	
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+	public double getPreco() {
+		return preco;	
+	}
+
+
+	public boolean getCodStatus() {
+		return codStatus;
+	}
+
+
+	public void setCodStatus(boolean codStatus) {
+		this.codStatus = codStatus;
+	}
 	
 	
 	
